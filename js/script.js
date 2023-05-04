26b7763fccb78ed5876ccfa10542e427103ab6ad
MuriloNogr <rm89162@fiap.com.br>
2023-05-03 22:56:22 -0300

    Button-Rgb com addEventListener Math getById

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-var nome1 = "Petroni";
+// var nome1 = "Petroni";
 // let nome2 = "Derick";
 // const nome3 = "Juan";
 
-if(nome1 != "") {
-let nome1 = "Geovana";
-}
+// if(nome1 != "") {
+// let nome1 = "Geovana";
+// }
 
-console.log(nome1);
+// console.log(nome1);
+
+let botao = document.getElementById("meu-btn");
+botao.addEventListener("click", function(){
+    // console.log(this.textContent);
+    let r=0,g=0,b=0;
+    r= Math.round(Math.random()*255);
+    g= Math.round(Math.random()*255);
+    b= Math.round(Math.random()*255);
+
+    // this.setAttribute("style","background-color:rgb("+r+", "+g+", "+b+");");
+    this.setAttribute("style",`background-color:rgb(${r},${g},${b});`);
+} );
