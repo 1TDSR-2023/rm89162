3ce5c87f24f7942d53031c0204015ba9da0ad9bc
MuriloNogr <rm89162@fiap.com.br>
2023-05-05 22:15:49 -0300

    Array declarations

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
-var nome1 = "Petroni";
-// let nome2 = "Derick";
-// const nome3 = "Juan";
+// let frutas = ["banana", "limão","laranja", "tomate", "abacaxi", "melancia"];
+// console.log(frutas);
+// console.table(frutas);
+// console.log(frutas[0]);
+// console.log(frutas[6]);
 
-if(nome1 != "") {
-let nome1 = "Geovana";
-}
+// frutas.push("manga");
+// console.log(frutas);
 
-console.log(nome1);
+// frutas.unshift("pitaya");
+// console.log(frutas);
+
+// frutas.pop();
+// console.log(frutas);
+
+// frutas.shift();
+// console.log(frutas);
+
+// // let indice = frutas.indexOf("laranja");
+// // console.log(`O indice encontrado foi: ${indice}`);
+// // console.log(`O item buscado foi: ${frutas[indice]}`);
+
+let indice = frutas.indexOf("melancia")
+console.log(`O item encontrado no indice antes da remoção foi: ${frutas[indice]}`);
+frutas.splice(indice,1);
+console.log(frutas);
+console.log(`o item encontrado no indice depois da remoção foi: ${frutas[indice]}`);
