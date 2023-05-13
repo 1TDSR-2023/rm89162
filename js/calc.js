a3374314407ba15d2293f160f49fa336eb7df97d
MuriloNogr <rm89162@fiap.com.br>
2023-05-12 23:13:49 -0300

    calculadora

---
+++ js/calc.js
@@ -0,0 +1,12 @@
+function insereNumeroNoVisor(botao, visor) {
+    if(botao.value == "+" || botao.value == "-" || botao.value == "*" || botao.value == "/"){
+        
+        if(visor.value[visor.value.lenght-1] == "+" || if(visor.value[visor.value.lenght-1] == "-" || if(visor.value[visor.value.lenght-1] == "*" || if(visor.value[visor.value.lenght-1] == "/") 
+    }
+}
+
+const calculoDoVisor = (visor)=>{
+    visor.value = eval(visor.value) 
+}
+
+
