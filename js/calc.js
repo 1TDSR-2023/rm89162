cafa75f3c60461c02fcc749ae6f136f1b91046ba
MuriloNogr <rm89162@fiap.com.br>
2023-05-19 01:58:13 -0300

    Login

--- js/calc.js
+++ js/calc.js
@@ -1,12 +1,49 @@
-function insereNumeroNoVisor(botao, visor) {
-    if(botao.value == "+" || botao.value == "-" || botao.value == "*" || botao.value == "/"){
+//Desafio 1
+//Utilizando uma função com passagem de parâmetros insira os números e operadores matemáticos no visor.
+//Você pode se utilizar de eventos inline no html.
+//Boa sorte você tem 5 minutos.
+
+function insereNumeroNoVisor(botao,visor) {
+    if(botao.value == "+" || botao.value == "-" || botao.value == "*" || botao.value == "/" ){
+
+        if(visor.value[visor.value.length-1] == "+" || visor.value[visor.value.length-1] == "-" || visor.value[visor.value.length-1] == "*" || visor.value[visor.value.length-1] == "/"){
+            visor.value.slice(visor.value.length-1)
+        }else{
+            visor.value += botao.value
+        }
         
-        if(visor.value[visor.value.lenght-1] == "+" || if(visor.value[visor.value.lenght-1] == "-" || if(visor.value[visor.value.lenght-1] == "*" || if(visor.value[visor.value.lenght-1] == "/") 
+    }else{
+        visor.value += botao.value
     }
+
 }
+
+//Desafio 2
+//Utilizando a função eval(expressão) calcule o resultado do visor.
+//Atrele o evento ao botão de IGUAL.
+//Crie uma função para executar este procedimento.
+//A função deve ser do tipo ARROW e deve receber um parâmetro!!!
+
+//O FORMATO DA FUNÇÃO ARROW É:
+// declarador identificador = ()=>{ }
 
 const calculoDoVisor = (visor)=>{
-    visor.value = eval(visor.value) 
+    visor.value = eval(visor.value);
 }
 
+
+// function fn1() {
+//     console.log(this.id);
+// }
+
+// const fn2 = ()=> {
+//     console.log(botaoResult.id);
+// }
+
+// const botaoResult = document.querySelector("#btnResultado");
+
+// botaoResult.addEventListener("click", fn2);
+
+//Desafio 3
+//Impeça a entrada de mais de uma vez do operador matemático.
 
--- js/login.js
+++ js/login.js
@@ -4,10 +4,11 @@ inputEmail.addEventListener("focus", ()=>{
     inputEmail.setAttribute("style", "outline-color: #ff0000;");
 });
 
+const inputSenha = document.querySelector("#idSenha");
 inputEmail.addEventListener("keyup", ()=>{
-
+    
     const lblEmail = document.querySelector("label[for='idEmail']");
-
+    
     if(inputEmail.value.length < 5) {
         inputEmail.setAttribute("style", "outline-color: #ff0000;");
         lblEmail.setAttribute("style", "color:#ff0000;");
@@ -22,7 +23,6 @@ const eyePass = document.querySelector(".fa-eye");
 
 eyePass.addEventListener("click", ()=>{
     
-    const inputSenha = document.querySelector("#idSenha");
 
     if(inputSenha.getAttribute("type") == "password") {
         inputSenha.setAttribute("type", "text");
@@ -32,3 +32,102 @@ eyePass.addEventListener("click", ()=>{
         eyePass.setAttribute("class", "fa fa-eye");
     }
 })
+
+// const usuario1 = {
+//     nomeCompleto : "Murilo Nogueira",
+//     emailUsuario : "murilo@email.com",
+//     senhaUsuario : "123456"
+// }
+// const usuario2 = {
+//     nomeCompleto : "asfsaf Nogueira",
+//     emailUsuario : "asfsaf@email.com",
+//     senhaUsuario : "654321"
+// }
+
+// let listaDeUsuarios = [];
+// listaDeUsuarios.push(usuario1);
+// listaDeUsuarios.push(usuario2);
+
+
+let listaDeUsuarios = [
+
+    {
+        nomeCompleto : "Murilo Nogueira",
+        emailUsuario : "murilo@email.com",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "Junior Nogueira",
+        emailUsuario : "junior@email.com",
+        senhaUsuario : "654321"
+    }
+    ,
+    {
+        nomeCompleto : "Filho Nogueira",
+        emailUsuario : "filho@email.com",
+        senhaUsuario : "654321"
+    }
+    ,
+    {
+        nomeCompleto : "Neto Nogueira",
+        emailUsuario : "neto@email.com",
+        senhaUsuario : "654321"
+    }
+    ,
+    {
+        nomeCompleto : "Segundo Nogueira",
+        emailUsuario : "segundo@email.com",
+        senhaUsuario : "654321"
+    }
+]
+    
+    
+
+
+const botaoSubmit = document.querySelector("#btnSubmit");
+
+botaoSubmit.addEventListener("click", ()=>{
+
+let userValidado = {
+        emailUsuarioValidado : inputEmail.value,
+        senhaUsuarioValidado : inputEmail.value
+    }
+
+    try{
+        listaDeUsuarios.forEach((usuario)=>{
+            if(userValidado.emailUsuarioValidado == usuario.emailUsuario && userValidado.senhaUsuarioValidado == usuario.senhaUsuario) {
+                userValidado = usuario;
+                throw "VALIDADO";
+            }
+        });
+
+        throw "SENHA INVALIDA"
+
+    } catch (msg) {
+
+        const msgStatus = document.querySelector("#msgStatus");
+
+        if(msg == "VALIDADO") {
+            msgStatus.setAttribute("style", "color: #00ff00");
+            msgStatus.innerHTML = `<span> <strong>Usuario: ${userValidado.nomeCompleto},Login efetuado com sucesso! </strong> </span>`
+        } else {
+            msgStatus.setAttribute("style", "color: #ff0000");
+            msgStatus.innerHTML = "<span> <strong>Nome de usuario ou senha invalidos! </strong> </span>"
+        }
+    }
+
+    // listaDeUsuarios.forEach((usuario)=>{
+
+    //     if(userValidado.emailUsuarioValidado == usuario.emailUsuario && userValidado.senhaUsuarioValidado == usuario.senhaUsuario) {
+    //         userValidado = usuario;
+    //     }
+    // });
+
+    // if(userValidado.emailUsuarioValidado == inputEmail.value && userValidado.senhaUsuarioValidado == inputSenha.value) {
+    //     console.log("USUARIO VALIDADO!")
+    // } else {
+    //     console.log("SENHA OU NOME DE USUARIOS INVALIDOS!")
+    // }
+
+});
